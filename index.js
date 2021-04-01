cff7ede3d966976ab64ba4cb75288cae3fca06a6
Gabriel Vasconcellos <gavapim@gmail.com>
2021-04-01 09:28:00 -0300

    Backend find all

--- index.js
+++ index.js
@@ -24,7 +24,11 @@ app.use(bodyParser.json());
 
 //routes
 app.get("/", (req, res) => {
-    res.render('index');
+    pergunta.findAll({raw: true}).then(perguntas => {
+        res.render('index', {
+            perguntas: perguntas
+        });
+    });
 });
 
 app.get("/perguntar", (req, res) => {
