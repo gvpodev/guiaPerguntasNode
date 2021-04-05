2e181ecd0249f400527667733ee248f9f4d843c8
Gabriel Vasconcellos <gavapim@gmail.com>
2021-04-05 09:12:47 -0300

    Page de pergunta atraves da url

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/database');
 const pergunta = require('./database/Pergunta');
+const Pergunta = require('./database/Pergunta');
 
 //database
 connection
@@ -24,7 +25,9 @@ app.use(bodyParser.json());
 
 //routes
 app.get("/", (req, res) => {
-    pergunta.findAll({raw: true}).then(perguntas => {
+    pergunta.findAll({raw: true, order: [
+        ['id', 'DESC'] // order by id (asc || desc)
+    ]}).then(perguntas => {
         res.render('index', {
             perguntas: perguntas
         });
@@ -44,6 +47,21 @@ app.post("/salvarpergunta", (req, res) => {
     }).then(() => {
         res.redirect('/');
     })
+});
+
+app.get("/pergunta/:id", (req, res) => {
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta => {
+        if (pergunta != undefined) {
+            res.render('pergunta', {
+                pergunta: pergunta
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
 
